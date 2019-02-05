95b55feb4cdd0c9d55b465a42dd2cbe0b0eaefff
sdixit03 <40880273+sdixit03@users.noreply.github.com>
2019-02-05 12:00:05 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -3,9 +3,13 @@ if (char>='a' and char<='z') or (char>='A' and char<='Z'):
     if(char=='a' or char=='e' or char=='i' or char=='o' or char=='u' or char=='A' or char=='E' or char=='I' or char=='O' or char=='U'):
         print("Vowel")
     else:
-        print("Consonent")
+        print("Consonant")
 else:
     print("invalid")
 
+    
+    
+
+    
     
     
